be53cd831b228e192234c8c8ee74cf57fd5ce420
yangshilin <734903690@qq.com>
2019-11-20 15:27:29 +0800

    third commit by idea

--- GitTest/src/supermap/ysl/Hello.java
+++ GitTest/src/supermap/ysl/Hello.java
@@ -2,6 +2,8 @@ package supermap.ysl;
 
 public class Hello {
     public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
         System.out.println("Hello !!!");
     }
 }
