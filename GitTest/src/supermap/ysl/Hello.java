c354232544ae53e6bd0ef68d88eae00cdbf80a87
yangshilin <734903690@qq.com>
2019-11-20 15:34:36 +0800

    fourth

--- GitTest/src/supermap/ysl/Hello.java
+++ GitTest/src/supermap/ysl/Hello.java
@@ -3,7 +3,9 @@ package supermap.ysl;
 public class Hello {
     public static void main(String[] args) {
         int a = 10;
+        String b = "哈哈";
         System.out.println(a);
+        System.out.println(b+a);
         System.out.println("Hello !!!");
     }
 }
